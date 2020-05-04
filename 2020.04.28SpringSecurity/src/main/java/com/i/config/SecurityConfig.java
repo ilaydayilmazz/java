aaedf0422c3cba2adaae672f4cc540a2878e3102
Administrator <Administrator@DESKTOP-A2E32AF>
2020-05-04 11:27:36 +0300

    Spring Security

---
+++ 2020.04.28SpringSecurity/src/main/java/com/i/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.i.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users=User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser(users.username("iy").password("m").roles("ADMIN"))
+		.withUser(users.username("a").password("b").roles("PERSONEL"))
+		.withUser(users.username("k").password("y").roles("PERSONEL"));
+		// TODO Auto-generated method stub
+		//super.configure(auth);
+	}
+
+}
